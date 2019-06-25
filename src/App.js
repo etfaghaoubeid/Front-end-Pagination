943cf8ed1dbce7c342a67e67c0db501f1c528754
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-06-25 11:50:25 +0000

    init app component state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 function App() {
+  const [isLoading,setIsLoading] = useState(false)
+  const [curentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+  const [posts,setPostes]=  useState([])
+  useEffect(()=>{
+    async function fetchPost(){
+       const response =  await fetch('https://jsonplaceholder.typicode.com/posts')
+       console.log(response)
+    }
+    fetchPost()
+  })
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>atigh</h1>
     </div>
   );
 }
